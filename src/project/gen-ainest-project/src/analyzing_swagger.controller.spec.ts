b4580745bfea1118ab9365b1b0bef07055e8fbdc
prakashshnu10 <prakashs@nu10.co>
2024-08-07 16:02:19 +0530

    done

---
+++ src/project/gen-ainest-project/src/analyzing_swagger.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AnalyzingSwaggerController } from './app/analyzing_swagger.controller';
+import { AnalyzingAPIVersioningService } from './app/analyzing.service';
+
+describe('AnalyzingSwaggerController', () => {
+  let appController: AnalyzingSwaggerController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AnalyzingSwaggerController],
+      providers: [AnalyzingAPIVersioningService],
+    }).compile();
+
+    appController = app.get<AnalyzingSwaggerController>(AnalyzingSwaggerController);
+  });
+
+  describe('getHelloV1', () => {
+    it('should return "Hello from API v1!"', () => {
+      expect(appController.getHelloV1()).toBe('Hello from API v1!');
+    });
+  });
+
+  describe('getHelloV2', () => {
+    it('should return "Hello from API v2!"', () => {
+      expect(appController.getHelloV2()).toBe('Hello from API v2!');
+    });
+  });
+});
